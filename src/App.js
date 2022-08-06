aea347767d4a299d78aa5b124bf66e2bb9cb32eb
Arunim Chakraborty <arunimchakraborty@gmail.com>
2022-08-06 13:04:48 +0530

    completed login screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Login from "./components/Login";
+import { MantineProvider } from "@mantine/core";
+
 function App() {
-	return <div>Hellu</div>;
+	return (
+		<MantineProvider withGlobalStyles withNormalizeCSS>
+			<Login />
+		</MantineProvider>
+	);
 }
 
 export default App;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,31 @@
+import { Input } from '@mantine/core';
+import { IconAt, IconEyeCheck, IconEyeOff } from '@tabler/icons';
+import { PasswordInput } from '@mantine/core';
+import { Title } from '@mantine/core';
 export default function Login(){
     return(
-        <div>Hello this is login yo</div>
+        <div style={{paddingTop: 20, paddingBottom: 20, paddingLeft: 30, paddingRight: 30}}>
+            <div style={{display: "flex", width: "100%", justifyContent: "center",paddingTop: 20, paddingBottom: 40 }}>
+                <Title order={1}>Login</Title>
+            </div>
+            <div style={{paddingTop: 20, paddingBottom: 20,}}>
+                <Input
+                icon={<IconAt />}
+                placeholder="Your email"
+                />
+            </div>
+            <div style={{paddingTop: 20, paddingBottom: 20,}}>
+                <PasswordInput
+                placeholder="Password"
+                label="Password"
+                description="Password must include at least one letter, number and special character"
+                required
+                defaultValue="secret"
+                visibilityToggleIcon={({ reveal, size }) =>
+                reveal ? <IconEyeOff size={size} /> : <IconEyeCheck size={size} />
+      }
+                />
+            </div>
+        </div>
     )
 }
